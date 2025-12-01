4ae93702e66b883859930083f0f83cafeb36edec
Raghunandan Pratoori <rnp@iastate.edu>
2025-12-01 12:45:19 -0600

    Fix compiler warnings
    - Added override keywords to InitialValues::value()
    - Added override keywords to BoundaryDisplacement methods
    - Marked unused parameter 'p' in InitialValues::value()
    - Build now completes with zero warnings

--- include/boundarydisplacement.h
+++ include/boundarydisplacement.h
@@ -4,75 +4,62 @@
 #include "dealiiheaders.h"
 #include "initialvalues.h"
 
+// Applying displacement on the boundary if periodic condition is absent.
+// Displacement on the bpoundary can be alternatively applied through periodic
+// condition as well.
 
-//Applying displacement on the boundary if periodic condition is absent.
-//Displacement on the bpoundary can be alternatively applied through periodic condition as well.
+namespace PhaseField {
+using namespace dealii;
 
-namespace PhaseField
-{
-  using namespace dealii;
+typedef TrilinosWrappers::MPI::Vector vectorType;
+typedef TrilinosWrappers::SparseMatrix matrixType;
 
-  typedef TrilinosWrappers::MPI::Vector vectorType;
-  typedef TrilinosWrappers::SparseMatrix matrixType;
+template <int dim> class BoundaryDisplacement : public Function<dim> {
+public:
+  BoundaryDisplacement(const double direction, const int timestep);
+  virtual void vector_value(const Point<dim> &p,
+                            Vector<double> &values) const override;
+  virtual void
+  vector_value_list(const std::vector<Point<dim>> &points,
+                    std::vector<Vector<double>> &value_list) const override;
 
-template <int dim>
-  class BoundaryDisplacement :  public Function<dim>
-  {
-  public:
-    BoundaryDisplacement (const double direction, const int timestep);
-    virtual
-    void
-    vector_value (const Point<dim> &p,
-                  Vector<double>   &values) const;
-    virtual
-    void
-    vector_value_list (const std::vector<Point<dim> > &points,
-                       std::vector<Vector<double> >   &value_list) const;
-  private:
-    const double direction;
-    const int timestep;
-  };
+private:
+  const double direction;
+  const int timestep;
+};
 
 template <int dim>
-BoundaryDisplacement<dim>::BoundaryDisplacement (const double direction, const int timestep)
-  :
-  Function<dim> (dim),
-  direction(direction),
-  timestep(timestep)
-{}
+BoundaryDisplacement<dim>::BoundaryDisplacement(const double direction,
+                                                const int timestep)
+    : Function<dim>(dim), direction(direction), timestep(timestep) {}
 
 template <int dim>
-inline
-void
-BoundaryDisplacement<dim>::vector_value (const Point<dim> &/*p*/,
-                              Vector<double>   &values) const
-{
-  Assert (values.size() == dim,
-          ExcDimensionMismatch (values.size(), dim));
+inline void
+BoundaryDisplacement<dim>::vector_value(const Point<dim> & /*p*/,
+                                        Vector<double> &values) const {
+  Assert(values.size() == dim, ExcDimensionMismatch(values.size(), dim));
 
   values = 0.0;
-  //values(direction)=-5e-4;
+  // values(direction)=-5e-4;
 
-  if(timestep<70)
-      values(direction)=-1.5e-3;
-  else if(timestep%10==0)
-      values(direction)=-1.5e-3;
+  if (timestep < 70)
+    values(direction) = -1.5e-3;
+  else if (timestep % 10 == 0)
+    values(direction) = -1.5e-3;
   else
-      values(direction)=0.0;
+    values(direction) = 0.0;
 }
 
 template <int dim>
-void
-BoundaryDisplacement<dim>::vector_value_list (const std::vector<Point<dim> > &points,
-                                   std::vector<Vector<double> >   &value_list) const
-{
+void BoundaryDisplacement<dim>::vector_value_list(
+    const std::vector<Point<dim>> &points,
+    std::vector<Vector<double>> &value_list) const {
   const unsigned int n_ooints = points.size();
-  Assert (value_list.size() == n_ooints,
-          ExcDimensionMismatch (value_list.size(), n_ooints));
-  for (unsigned int p=0; p<n_ooints; ++p)
-      BoundaryDisplacement<dim>::vector_value (points[p],
-                                  value_list[p]);
+  Assert(value_list.size() == n_ooints,
+         ExcDimensionMismatch(value_list.size(), n_ooints));
+  for (unsigned int p = 0; p < n_ooints; ++p)
+    BoundaryDisplacement<dim>::vector_value(points[p], value_list[p]);
 }
 
-}
+} // namespace PhaseField
 #endif
--- include/initialvalues.h
+++ include/initialvalues.h
@@ -4,51 +4,42 @@
 #include "dealiiheaders.h"
 #include "initialvalues.h"
 
-
 // defines the initial condition for the order parameter
 
-namespace PhaseField
-{
-  using namespace dealii;
+namespace PhaseField {
+using namespace dealii;
 
-  typedef TrilinosWrappers::MPI::Vector vectorType;
-  typedef TrilinosWrappers::SparseMatrix matrixType;
+typedef TrilinosWrappers::MPI::Vector vectorType;
+typedef TrilinosWrappers::SparseMatrix matrixType;
 
-template <int dim>
-class InitialValues : public Function<dim>
-{
+template <int dim> class InitialValues : public Function<dim> {
 public:
- InitialValues (const int &variant,const int &time_step)
-  :
-   Function<dim>(),
-     variant (variant),
-   time_step (time_step)
+  InitialValues(const int &variant, const int &time_step)
+      : Function<dim>(), variant(variant), time_step(time_step)
 
-    {}
- virtual double value(const Point<dim>   &p,
-                       const unsigned int  /*component = 0*/) const;
+  {}
+  virtual double value(const Point<dim> &p,
+                       const unsigned int /*component = 0*/) const override;
+
 private:
- const int variant ;
- const int time_step ;
-
+  const int variant;
+  const int time_step;
 };
 
 template <int dim>
-double InitialValues<dim>::value (const Point<dim>  &p,
-                           const unsigned int /*component*/) const
-{
+double InitialValues<dim>::value(const Point<dim> & /*p*/,
+                                 const unsigned int /*component*/) const {
 
-//               if (pow(p[0]-35,2)+pow(p[1]-10,2)<4)
-//                {
-//                   if (variant==1)
-//                       return 0.01;
-//                   else
-//                       return 0;
-//                }
-//               else if (time_step==0)
-        return 0.0;
-
+  //               if (pow(p[0]-35,2)+pow(p[1]-10,2)<4)
+  //                {
+  //                   if (variant==1)
+  //                       return 0.01;
+  //                   else
+  //                       return 0;
+  //                }
+  //               else if (time_step==0)
+  return 0.0;
 }
 
-}
+} // namespace PhaseField
 #endif
